c6055f32ba67917a29e09fba19d228fa6554bad3
Aaron Brown <aaron.j.brown90@gmail.com>
2021-01-25 11:49:20 +1000

    Add config description for Netmask

--- scripts/Growatt.lua
+++ scripts/Growatt.lua
@@ -32,6 +32,7 @@ local config_id_types = {
   [0x14] = "Device Type",
   [0x15] = "Software Version",
   [0x16] = "Hardware Version",
+  [0x19] = "Netmask",
   [0x1e] = "Timezone",
   [0x1f] = "Date"
 }
